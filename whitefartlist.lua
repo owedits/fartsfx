ea59d1358ec6f904fb5eb620ef8477a187f4f0b7
owedits <89522931+owedits@users.noreply.github.com>
2021-11-07 00:32:28 +0300

    f

--- whitefartlist.lua
+++ whitefartlist.lua
@@ -1,38 +1,5 @@
-
-local premiumList = {
-  [320664513] = "KrystekYT", -- KrystekYT
-  [294992884] = "DjRudzik", -- DjRudzik
-  [2898280324] = "Weronika9896", -- Weronika9896
-  [1764348997] = "Kacpero202", -- Kacpero202
-  [639977402] = "DawajSernik", -- DawajSernik
-  [531348383] = "Wariatka1337", -- Wariatka1337
-  [1100409110] = "Polska_Kielbaska", -- Polska_Kielbaska
-  [1068992223] = "tutaccount", -- tutaccount
-  [2433227342] = "KubaGames2115", -- KubaGames2115
-  [2854035659] = "NobekWloluu", -- NobekWloluu
-  [652382788] = "lewowity", -- lewowity
-  [734725590] = "WiktorGames32", -- WiktorGames32
-  [761017630] = "bobejlk", -- bobejlk
-  [2864331664] = "PolCher1", -- PolCher1
-  [2939661997] = "Fan_Yuujina", -- Fan_Yuujina
-  [2624963365] = "Hnea1338v2", -- Hnea1338v2
-  [1173490265] = "xykacper", -- xykacper
-  [1038425504] = "KubaJeden337", -- KubaJeden337
-  [720781459] = "Kuraworkout123", -- Kuraworkout123
-  [1038290884] = "tymowie", -- tymowie
-  [473416850] = "JedrekGir4", -- JedrekGir4
-  [417336375] = "MrKoxDominik", -- MrKoxDominik
-  [2866274812] = "OrderClaim3",
-  [2712629196] = "istudy7",
-  [344467002] = "OskarKoxpp",
-  [363550003] = "oliwiergapa12345",
-  --[] = "",
-  --[] = "",
-  --[] = "",
-  --[] = "",
-  --[] = "",
-  --[] = "",
-  --[] = "",
-}
-return premiumList
---
+local whitelisted = ("istudy7")
+local whitelisted2 = ("daredder12")
+local whitelisted3 = ("xxxtentaci6n")
+local whitelisted4 = ("dhndahadhnbt")
+local whitelisted5 = ("empty")
