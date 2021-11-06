5b8ec4b0ecbe8ea95b980a640e2955d7a8cb7c63
owedits <89522931+owedits@users.noreply.github.com>
2021-11-06 21:37:51 +0300

    dd

---
+++ whtfa.lua
@@ -0,0 +1,28 @@
+userids = {
+[320664513] = true, -- KrystekYT
+[294992884] = true, -- DjRudzik
+[2898280324] = true, -- Weronika9896
+[1764348997] = true, -- Kacpero202
+[639977402] = true, -- DawajSernik
+[531348383] = false, -- Wariatka1337
+[1100409110] = true, -- Polska_Kielbaska
+[1068992223] = true, -- tutaccount
+[2433227342] = true, -- KubaGames2115
+[2854035659] = true, -- NobekWloluu
+[652382788] = true, -- lewowity
+[734725590] = true, -- WiktorGames32
+[761017630] = true, -- bobejlk
+[2864331664] = true, -- PolCher1
+[2939661997] = true, -- Fan_Yuujina
+[2624963365] = true, -- Hnea1338v2
+[1173490265] = true, -- xykacper
+[1038425504] = true, -- KubaJeden337
+[720781459] = true, -- Kuraworkout123
+[1038290884] = true, -- tymowie
+[473416850] = true, -- JedrekGir4
+[417336375] = true, -- MrKoxDominik
+[2712629196] = true, -- istudy7
+--[] = true, -- 
+}
+return userids
+----
