ab26d0a0ed9ed1f55af0d04663f7e0db6402fcf4
owedits <89522931+owedits@users.noreply.github.com>
2021-11-08 16:46:00 +0300

    f

---
+++ err.lua
@@ -0,0 +1,80 @@
+local whitelisted = ("istudy7") -- permanent
+local whitelisted2 = ("dared") -- permanent 
+local whitelisted3 = ("xxxtentaci6n") -- permanent
+local whitelisted4 = ("dhndahadhnbt") -- permanent
+local whitelisted5 = ("WiesoBistnSuus") -- permanent
+local whitelisted6 = ("WiesoBistnMythical") -- permanent
+local whitelisted7 = ("WiesoBistnDarkMatter") -- permanent
+local whitelisted8 = ("WhitebeardAces") -- permanent
+local whitelisted9 = ("TIMBERVIEWS") -- permanent
+local whitelisted10 = ("callshawn") -- permanent
+local whitelisted11 = ("iQzrr") -- permanent
+local whitelisted12 = ("MightBeMeButMightNot") -- permanent
+local whitelisted13 = ("s4idzy") -- permanent
+local whitelisted14 = ("jovkim100") -- permanent
+local whitelisted15 = ("RobloxKinqsThieves") -- permanent
+local whitelisted16 = ("baoisabot2") -- dingo
+local whitelisted17 = ("Hawk_Salvatore") -- permanent
+local whitelisted18 = ("Raphyboi_v3") -- permanent
+local whitelisted19 = ("Kumarci97") -- permanent
+local whitelisted20 = ("Saber7530") -- permanent
+local whitelisted21 = ("misterjamze") -- permanent
+local whitelisted22 = ("yt_gamer992") -- permanent
+local whitelisted23 = ("satvik17211") -- permanent
+local whitelisted24 = ("FatalSabers") -- permanent
+local whitelisted25 = ("CrxsaderZ") -- permanent
+local whitelisted26 = ("NickIlin15") -- permanenttest
+local whitelisted27 = ("henrysherbert") -- permanent
+local whitelisted28 = ("iQzrr") -- permanent
+local whitelisted29 = ("XiaoTianchuang") -- perma maybe
+local whitelisted30 = ("ThreeDevils") -- permanent
+local whitelisted31 = ("elymoshiro19") -- vouch test now permanent
+local whitelisted32 = ("Geno_Chouinard544") -- permanent
+local whitelisted33 = ("MelihAga98") -- yiitarkadas
+local whitelisted34 = ("ReesyNY") -- permanent
+local whitelisted35 = ("newnoooob1231") -- perma
+local whitelisted36 = ("Tss") -- permanent
+local whitelisted37 = ("ss9") -- vouch test now permanent
+local whitelisted38 = ("ss") -- permanent
+local whitelisted39 = ("ss") -- yiitarkadas
+local whitelisted40 = ("ss") -- permanent
+local whitelisted41 = ("ss") -- perma
+local whitelisted42 = ("ss") -- perma
+local whitelisted43 = ("ss") -- perma
+local whitelisted44 = ("ss") -- perma
+local whitelisted45 = ("ss") -- perma
+local whitelisted46 = ("ss") -- perma
+local whitelisted47 = ("ss") -- perma
+local whitelisted48 = ("ss") -- perma
+local whitelisted49 = ("ss") -- perma
+local whitelisted50 = ("ss") -- perma
+local whitelisted51 = ("ss") -- perma
+local whitelisted52 = ("ss") -- perma
+local whitelisted53 = ("ss") -- perma
+local whitelisted54 = ("ss") -- perma
+local whitelisted55 = ("ss") -- perma
+local whitelisted56 = ("ss") -- perma
+local whitelisted57 = ("ss") -- perma
+local whitelisted58 = ("ss") -- perma
+local whitelisted59 = ("ss") -- perma
+local whitelisted60 = ("ss") -- perma
+local whitelisted61 = ("ss") -- perma
+
+
+
+ 
+function script()
+print("Starting...")
+ 
+end
+ 
+local nonamexd = game.Players.LocalPlayer.Name
+if nonamexd == whitelisted or nonamexd == whitelisted2 or nonamexd == whitelisted3 or nonamexd == whitelisted4 or nonamexd == whitelisted5 or nonamexd == whitelisted6 or nonamexd == whitelisted7 or nonamexd == whitelisted8 or nonamexd == whitelisted9 or nonamexd == whitelisted10 or nonamexd == whitelisted11 or nonamexd == whitelisted12 or nonamexd == whitelisted13 or nonamexd == whitelisted14 or nonamexd == whitelisted15 or nonamexd == whitelisted16 or nonamexd == whitelisted17 or nonamexd == whitelisted18 or nonamexd == whitelisted19 or nonamexd == whitelisted20 or nonamexd == whitelisted21 or nonamexd == whitelisted22 or nonamexd == whitelisted23 or nonamexd == whitelisted24 or nonamexd == whitelisted25 or nonamexd == whitelisted26 or nonamexd == whitelisted27 or nonamexd == whitelisted28 or nonamexd == whitelisted29 or nonamexd == whitelisted30 or nonamexd == whitelisted31 or nonamexd == whitelisted32 or nonamexd == whitelisted33 or nonamexd == whitelisted34 or nonamexd == whitelisted35 or nonamexd == whitelisted36 or nonamexd == whitelisted37 or nonamexd == whitelisted38 or nonamexd == whitelisted39 or nonamexd == whitelisted40 or nonamexd == whitelisted41 or nonamexd == whitelisted42 or nonamexd == whitelisted43 or nonamexd == whitelisted44 or nonamexd == whitelisted45 or nonamexd == whitelisted46 or nonamexd == whitelisted47 or nonamexd == whitelisted48 or nonamexd == whitelisted49 or nonamexd == whitelisted50 or nonamexd == whitelisted51 or nonamexd == whitelisted52 or nonamexd == whitelisted53 or nonamexd == whitelisted54 or nonamexd == whitelisted55 or nonamexd == whitelisted56 or nonamexd == whitelisted57 or nonamexd == whitelisted58 or nonamexd == whitelisted59 or nonamexd == whitelisted60 or nonamexd == whitelisted61 then
+wait(1)
+script()
+warn("You are whitelisted. Welcome.")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/owedits/fartsfx/main/mainfart.lua'))()
+else
+wait(2)
+game.Players.LocalPlayer:kick "You are not whitelisted. Contact ow#3295 if you believe this is an error."
+end
